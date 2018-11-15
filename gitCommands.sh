6c92bd28fd08c25aa062eddfaa738edc72a5fd74
matiassambrizzi <msambrizzi@fi.uba.ar>
2018-11-15 12:06:40 -0300

    prueba

--- gitCommands.sh
+++ gitCommands.sh
@@ -1,8 +1,13 @@
-echo "Comandos git"
-echo "1 + "asd": Push"
+echo -e '\e[1;41mComandos Git\e[0m:'
+echo -e '\e[1;42mPara usarlo ingrese ./test.sh opcion "commit" "branch"\e[0m:'
+cat $1
+cat $2
+cat $3
 
-git add .
-git commit -m $1
-git push origin	$2
 
+if[$1 -eq 1]
+	git add .
+	git commit -m $2
+	git push origin	$3
+fi
 
