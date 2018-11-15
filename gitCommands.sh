409b1eed0b8add62158f8193b86eb966b6dc93f0
matiassambrizzi <msambrizzi@fi.uba.ar>
2018-11-15 12:23:29 -0300

    Ultimas

--- gitCommands.sh
+++ gitCommands.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
 echo -e '\e[1;41mComandos Git\e[0m:'
-echo -e '\e[1;42mPara usarlo ingrese ./test.sh opcion "commit" "branch"\e[0m:'
-cat $1
-cat $2
-cat $3
+echo -e '\e[1;42mPara usarlo ingrese ./gitCommands.sh opcion "commit" "branch"\e[0m:'
 
-
-if[$1 -eq 1]
+if [ $1 -eq 1 ]; then
 	git add .
 	git commit -m $2
 	git push origin	$3
+elif [ $1 -eq 2 ]; then
+	git pull origin $3 
+else
+	echo -e '\e[1;41mOpcion No Valida\e[0m:'
+	echo "Ingrese: 1 == PUSH"
+ 	echo "Para usarlo ingrese ./gitCommands.sh 1 "commit" "branch""
+	echo "Ingrese: 2 == PULL"
+	echo "Para usarlo ingrese ./gitCommands.sh 2 "commit" "branch""
 fi
 
